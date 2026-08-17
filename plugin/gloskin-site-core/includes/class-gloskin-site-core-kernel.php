ce916faedcfd80a0f4ef9d782dc9d1a62a3e3720
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-18 02:49:49 +0700

    sync Gloskin runtime version

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.122';
+	const VERSION = '0.7.123';
 
 	/** @var string */
 	private $plugin_file;
