d3317bed1d60a6bae114f89a77bc374d3d56f29c
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-20 16:07:16 +0700

    Bump active navigation release version [skip ci]

--- plugin/gloskin-site-core/gloskin-site-core.php
+++ plugin/gloskin-site-core/gloskin-site-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Gloskin Site Core
  * Description: Gloskin website presentation, content and integration runtime.
- * Version: 0.7.166
+ * Version: 0.7.167
  * Requires PHP: 7.4
  * Text Domain: gloskin-site-core
  */
