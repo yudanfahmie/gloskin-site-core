8173b661ce4b81297898a5e9ecc362542ae9f7dc
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-21 23:17:17 +0700

    Align asset version with 0.7.201

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.200';
+	const VERSION = '0.7.201';
 
 	/** @var string */
 	private $plugin_file;
