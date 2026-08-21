595c129e7365dbf41c8bd0535ad787ab203d6a0a
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-22 00:03:01 +0700

    Sync Gloskin release owner to 0.7.204

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.203';
+	const VERSION = '0.7.204';
 
 	/** @var string */
 	private $plugin_file;
