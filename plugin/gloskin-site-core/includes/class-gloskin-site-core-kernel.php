2fca17cea4250d7e366e1a875c48fe0c59838645
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-21 22:45:39 +0700

    Sync Home asset release version

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.198';
+	const VERSION = '0.7.199';
 
 	/** @var string */
 	private $plugin_file;
