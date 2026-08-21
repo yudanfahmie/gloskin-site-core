435961f24533749230b6c0e777681794befc63a3
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-21 16:06:07 +0700

    Release 0.7.187

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.186';
+	const VERSION = '0.7.187';
 
 	/** @var string */
 	private $plugin_file;
