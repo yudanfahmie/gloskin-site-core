2d723ca4a880050832b633e513fa672148f15eaf
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-22 16:46:13 +0700

    Bump kernel release version to 0.7.216

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.215';
+	const VERSION = '0.7.216';
 
 	/** @var string */
 	private $plugin_file;
