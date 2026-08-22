b225abbb4a4e93b2cd54df13d67326170e072840
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-22 21:20:10 +0700

    Align kernel release with 0.7.228

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.227';
+	const VERSION = '0.7.228';
 
 	/** @var string */
 	private $plugin_file;
