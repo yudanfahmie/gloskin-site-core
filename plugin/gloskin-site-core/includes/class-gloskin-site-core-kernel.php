6855183cd8a42229642df1c6122a7ee9a08fd47f
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-22 08:01:51 +0700

    Sync kernel to 0.7.210

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.209';
+	const VERSION = '0.7.210';
 
 	/** @var string */
 	private $plugin_file;
