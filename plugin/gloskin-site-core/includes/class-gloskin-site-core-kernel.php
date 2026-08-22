569dc0b424688e1da335a0a542cfb19b25f1ffe2
yudanfahmie <57941518+yudanfahmie@users.noreply.github.com>
2026-08-22 22:12:09 +0700

    chore: bump gloskin core to 0.7.231

--- plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
+++ plugin/gloskin-site-core/includes/class-gloskin-site-core-kernel.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 final class Gloskin_Site_Core_Kernel {
-	const VERSION = '0.7.230';
+	const VERSION = '0.7.231';
 
 	/** @var string */
 	private $plugin_file;
